97ad7d80e44a545b5c63d7ea0262a0abafae4811
zayennn <zaayeennn@gmail.com>
2026-01-13 22:03:49 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Menu from './components/Menu/Menu';
+import Camera from './components/Camera/Camera';
+import Upload from './components/Upload/Upload';
+import Final from './components/Final/Final';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/camera" element={<Camera />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/final" element={<Final />} />
+      </Routes>
+    </Router>
   );
 }
 
