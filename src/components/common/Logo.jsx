c202724698a551c0b32d5ce2150fa8a4cfbc48b5
zayennn <zaayeennn@gmail.com>
2026-01-13 22:11:32 +0700

    update

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -3,20 +3,20 @@ import { useNavigate } from 'react-router-dom';
 import styles from './Logo.module.css';
 
 const Logo = () => {
-  const navigate = useNavigate();
-  
-  const handleClick = () => {
-    navigate('/');
-  };
-  
-  return (
-    <div className={styles.logo} onClick={handleClick}>
-      <img 
-        src="/Assets/fish-photobooth/logo-new.png" 
-        alt="Nashallery's Booth Logo"
-      />
-    </div>
-  );
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/');
+    };
+
+    return (
+        <div className={styles.logo} onClick={handleClick}>
+            <img
+                src="/Assets/fish-photobooth/logo-new.png"
+                alt="Nashallery's Booth Logo"
+            />
+        </div>
+    );
 };
 
 export default Logo;
