ddba4bcc93ddfce75ef7cf2a72b299cde2cad55b
zayennn <zaayeennn@gmail.com>
2026-01-13 22:39:07 +0700

    update

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -3,35 +3,35 @@ import { useNavigate } from 'react-router-dom';
 import './Home.css';
 
 const Menu = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleLogoClick = () => {
-    navigate('/');
-  };
+    const handleLogoClick = () => {
+        navigate('/');
+    };
 
-  const handleCamera = () => {
-    navigate('/camera');
-  };
+    const handleCamera = () => {
+        navigate('/camera');
+    };
 
-  const handleUpload = () => {
-    navigate('/upload');
-  };
+    const handleUpload = () => {
+        navigate('/upload');
+    };
 
-  return (
-    <>
-      <div className="logo" onClick={handleLogoClick}>
-        <img src="Assets/fish-photobooth/logo-new.png" alt="Logo" />
-      </div>
+    return (
+        <>
+            <div className="logo" onClick={handleLogoClick}>
+                <img src="Assets/fish-photobooth/logo-new.png" alt="Logo" />
+            </div>
 
-      <div className="menu-container">
-        <div className="menu-header"></div>
-        <button id="menu-camera-button" onClick={handleCamera}>Take photos</button>
-        <button id="menu-upload-button" onClick={handleUpload}>Upload photos</button>
-      </div>
+            <div className="menu-container">
+                <div className="menu-header"></div>
+                <button id="menu-camera-button" onClick={handleCamera}>Take photos</button>
+                <button id="menu-upload-button" onClick={handleUpload}>Upload photos</button>
+            </div>
 
-      <script src="Javascripts/home.js" async></script>
-    </>
-  );
+            <script src="Javascripts/home.js" async></script>
+        </>
+    );
 };
 
 export default Menu;
