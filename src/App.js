0c6a2d03579439a768000f0be8f8782820c27762
zayennn <zaayeennn@gmail.com>
2026-01-13 22:31:46 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Menu from './components/Menu/Menu';
 import Camera from './components/Camera/Camera';
@@ -6,18 +6,31 @@ import Upload from './components/Upload/Upload';
 import Final from './components/Final/Final';
 import './App.css';
 
+const Logo = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="logo" onClick={() => navigate('/')}>
+      <img src="Assets/fish-photobooth/logo-new.png" alt="Logo" />
+    </div>
+  );
+};
+
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/camera" element={<Camera />} />
-        <Route path="/upload" element={<Upload />} />
-        <Route path="/final" element={<Final />} />
-      </Routes>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/camera" element={<Camera />} />
+          <Route path="/upload" element={<Upload />} />
+          <Route path="/final" element={<Final />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
 
+export { Logo };
 export default App;
